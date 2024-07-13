bab5459b55c45ca07aa57de05153901b0a2c8ad8
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-13 12:01:16 +0000

    [Fix](Uair): Removed `~` from paths.

--- Uair__/management.sh
+++ Uair__/management.sh
@@ -1,6 +1,6 @@
-. ~/.bookmarks/home-scripts/Uair__/consts.sh
+. "$HOME/.bookmarks/home-scripts/Uair__/consts.sh"
 
-uair --config ~/.bookmarks/home-configs/uair/work.toml --quiet &
+uair --config "$HOME/.bookmarks/home-configs/uair/work.toml" --quiet &
 # Get the most recently started process ID [get PID of a just started process](https://www.baeldung.com/linux/just-started-process-pid)|[Zotero](zotero://select/library/items/UD3GDWDD).
 echo $! > ${MANAGEMENT_PIDS_FILE}
 uairctl listen | yad --progress --no-buttons --css='* { font-size: 80px; }' &
